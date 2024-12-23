825de87a41669a181e2d5b26ac748ab725a4c2e0
omarfrihi <101578506+omarfrihi@users.noreply.github.com>
2024-12-23 18:56:52 +0100

    Update Popular.js

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import certificat from "../assets/certificat.jpg";
+import Card from "../components/card";
 
 export const Popular = () => {
   const [movieList, setMovieList] = useState([]);
@@ -97,49 +98,7 @@ export const Popular = () => {
 
         <div className="flex flex-wrap justify-around px-4 sm:px-14">
           {movieList.map((movie) => (
-            <div
-              key={movie.id}
-              className="max-w-sm border my-4 rounded-lg shadow bg-gray-800 border-gray-700"
-            >
-              <Link to={`/movie/${movie.id}`}>
-                <img
-                  className="rounded-t-lg"
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                />
-              </Link>
-              <div className="p-5">
-                <Link to={`/movie/${movie.id}`}>
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-                    {movie.title}
-                  </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-400">
-                  {tronquerChaine(movie.overview, 100)}
-                </p>
-                <Link
-                  to={`/movie/${movie.id}`}
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-rouge-1 rounded-lg hover:bg-rouge-2 focus:ring-4 focus:outline-none focus:ring-rouge-2"
-                >
-                  Read more
-                  <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </Link>
-              </div>
-            </div>
+            <Card {...movie} key={movie.id} />
           ))}
         </div>
       </section>
